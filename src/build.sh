bef44cc816915455e28712ea1c777b6752baa6d6
Ferenc Boldog <ferenc.boldog@binx.hu>
2025-05-12 10:45:38 +0200

    bugfix(sdcard): add extra flags for the build to avoid QT related build errors

--- src/build.sh
+++ src/build.sh
@@ -11,4 +11,7 @@ then
     SDV2=$2
 fi
 # run through grc to highlight NOTEs, WARNings and ERRORs
+export DISPLAY=0.0
+export QT_QPA_PLATFORM=offscreen
+export LD_LIBRARY_PATH=$GW_BIN/../lib:$LD_LIBRARY_PATH
 grc --config=gw_sh.grc gw_sh ./build${EXT}.tcl ${SDV2}
