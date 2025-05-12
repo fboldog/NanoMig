25155b162f85a2384b2c2bd491852f8f5cf78145
Ferenc Boldog <ferenc.boldog@binx.hu>
2025-05-12 10:44:17 +0200

    fearture(sdcard): update build sh to support v2 sdcard build

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 EXT=
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 1 ] || [ "$#" -eq 2 ]
+then
     EXT=_$1
 fi
 
+SDV2=
+if [ "$#" -eq 2 ]
+then
+    SDV2=$2
+fi
 # run through grc to highlight NOTEs, WARNings and ERRORs
-grc --config=gw_sh.grc gw_sh ./build${EXT}.tcl
+grc --config=gw_sh.grc gw_sh ./build${EXT}.tcl ${SDV2}
